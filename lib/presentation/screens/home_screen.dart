c937ee4c97c75b051674523495106b4679dd092c
dipondeb <developerdipon@gmail.com>
2024-09-16 23:38:59 +0600

    updated

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -5,6 +5,41 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(15),
+                child: Row(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: const Color(0xFFF9F5FD),
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Color(0xFF475269),
+                            blurRadius: 5,
+                            spreadRadius: 1,
+                          ),
+                        ]
+                      ),
+                      child: const Icon(
+                        Icons.sort,
+                        color: Color(0xFF475269),
+                        size: 30,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }

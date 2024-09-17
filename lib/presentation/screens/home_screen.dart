a6e2a1d9a001b2c40d2a168fb90945d26150c1a8
dipondeb <developerdipon@gmail.com>
2024-09-17 23:57:32 +0600

    updated

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:badges/badges.dart' as badges;
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -10,35 +11,107 @@ class HomeScreen extends StatelessWidget {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              Padding(
-                padding: const EdgeInsets.all(15),
-                child: Row(
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.all(8),
-                      decoration: BoxDecoration(
-                        color: const Color(0xFFF9F5FD),
-                        borderRadius: BorderRadius.circular(10),
-                        boxShadow: const [
-                          BoxShadow(
-                            color: Color(0xFF475269),
-                            blurRadius: 5,
-                            spreadRadius: 1,
-                          ),
-                        ]
-                      ),
-                      child: const Icon(
-                        Icons.sort,
-                        color: Color(0xFF475269),
-                        size: 30,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+              // build custom appBar
+              _buildCustomAppBar(),
+              const SizedBox(height: 15),
+              // build search bar
+              _buildSearchBar(),
             ],
           ),
         ),
+      ),
+    );
+  }
+  Widget _buildCustomAppBar() {
+    return Padding(
+      padding: const EdgeInsets.all(15),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+                color: const Color(0xFFF9F5FD),
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Color(0xFF475269),
+                    blurRadius: 5,
+                    spreadRadius: 1,
+                  ),
+                ]
+            ),
+            child: const Icon(
+              Icons.sort,
+              color: Color(0xFF475269),
+              size: 30,
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+                color: const Color(0xFFF9F5FD),
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Color(0xFF475269),
+                    blurRadius: 5,
+                    spreadRadius: 1,
+                  ),
+                ]
+            ),
+            child:  const badges.Badge(
+              badgeContent: Text(
+                '3',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              child: Icon(
+                Icons.notifications,
+                color: Color(0xFF475269),
+                size: 30,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSearchBar() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 15),
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      height: 55,
+      decoration: BoxDecoration(
+          color: const Color(0xFFF9F5FD),
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              color: const Color(0xFF475269).withOpacity(0.3),
+              blurRadius: 5,
+              spreadRadius: 1,
+            ),
+          ]
+      ),
+      child: Row(
+        children: [
+          Container(
+            width: 270,
+            child: TextFormField(
+              decoration: const InputDecoration(
+                border: InputBorder.none,
+                hintText: 'Search',
+              ),
+            ),
+          ),
+          const Icon(
+            Icons.search,
+            color: Color(0xFF475269),
+            size: 27,
+          ),
+        ],
       ),
     );
   }
